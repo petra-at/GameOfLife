b8c3786f671d54eeee062c7aad55bc5ef7438e76
petra-at <noreply>
2019-12-06 04:40:40 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameOfLife
+{
+    class GameOfLife
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
